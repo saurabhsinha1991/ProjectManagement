85916ab12b3b6da4565b19542716342795de4e3c
Saurabh <saurabh.sinha@walmartlabs.com>
2021-09-15 14:28:42 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,14 +9,17 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/api', (req, res) => {
-    res.send('API Running');
-});
-
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/register', require('./routes/api/register'));
 app.use('/api/projects', require('./routes/api/projects'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
