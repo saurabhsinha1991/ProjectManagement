8412fa8f7feb3ffbbe5a003b3559b64bddfd708f
Saurabh <saurabh.sinha@walmartlabs.com>
2021-09-15 08:31:37 +0530

    Added ag grid

--- client/src/containers/Project/index.js
+++ client/src/containers/Project/index.js
@@ -1,7 +1,7 @@
 import { AgGridColumn } from 'ag-grid-react/lib/agGridColumn';
 import { AgGridReact } from 'ag-grid-react/lib/agGridReact';
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import useProject from '../../hooks/useProject';
 
@@ -9,8 +9,7 @@ function Project() {
     const { id } = useParams();
     const [column, setColumn] = useState([]);
     const { getSingleProject } = useProject();
-
-    const rowData = [];
+    const [rowData, setRowData] = useState([]);
 
     useEffect(() => {
        const fetchData = async () => {
@@ -20,15 +19,30 @@ function Project() {
        fetchData();
     }, []);
 
+    const addRow = () => {
+        const defaultRow = {
+            title: '',
+            startDate: '',
+            endDate: '',
+            ...column.reduce((acc, item) => {
+                acc = { ...acc, [item.split(' ').join('')]: ''};
+                return acc;
+            }, {})
+        };
+
+        setRowData([...rowData, defaultRow]);
+    };
+
     return (
         <Container className="mt-20">
+            <Button className="mb-20" onClick={addRow}>Add Row</Button>
             <div className="ag-theme-alpine" style={{ height: 500, width: '100%'}}>
                 <AgGridReact rowData={rowData}>
-                    <AgGridColumn headerName="PHASE TITLE" field="title"></AgGridColumn>
-                    <AgGridColumn headerName="START DATE" field="startDate"></AgGridColumn>
-                    <AgGridColumn headerName="END DATE" field="endDate"></AgGridColumn>
+                    <AgGridColumn headerName="PHASE TITLE" editable field="title"></AgGridColumn>
+                    <AgGridColumn headerName="START DATE" editable field="startDate"></AgGridColumn>
+                    <AgGridColumn headerName="END DATE" editable field="endDate"></AgGridColumn>
                     {column.map((name) => (
-                        <AgGridColumn headerName={name} field={name.split(' ').join('')}></AgGridColumn>
+                        <AgGridColumn headerName={name} editable field={name.split(' ').join('')}></AgGridColumn>
                     ))}
                 </AgGridReact>
             </div>
